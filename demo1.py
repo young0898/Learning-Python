969a97802c3cb02b8db237b75e8e4a9448338746
young0898 <18389308036@139.com>
2018-02-25 00:23:36 +0800

    update demo1

--- demo1.py
+++ demo1.py
@@ -1,24 +1,30 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+os.chdir(os.getcwd()+"\data")  #切换到data目录
 
 def draw_ts(timeseries):
     timeseries.plot()
     plt.show()
 
 data = pd.read_csv("demo1.csv")
-#print data.shape
-#print data.head()
 
-data = data.set_index("No")
-draw_ts(data['A'])
+data = data.set_index("date")
+data.index = pd.to_datetime(data.index)
+print("data_shape:",data.shape)
+print(data.head())
+#draw_ts(data['A'])
 
 
-diff = data.diff().dropna()
+diff = data.diff(2)
+diff = diff.dropna()
+print("data_shape:",diff.shape)
+print(diff.head())
+
 td = diff.describe()
-print td['25%']
+print(type(td))
+
 
 #high = td['75%'] + 1.5 * (td['75%'] - td['25%'])
 #low = td['25%'] - 1.5 * (td['75%'] - td['25%'])
