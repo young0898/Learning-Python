adf91cee84c284d50e8af4e0909afcb4867883eb
young0898 <18389308036@139.com>
2018-02-25 21:25:17 +0800

    update

--- learn_matplotlib.py
+++ learn_matplotlib.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import numpy as np
 import matplotlib.pylab as plt
 
 os.chdir(os.getcwd() + "\data")
@@ -9,20 +8,24 @@ data = pd.read_excel("234Gliuliang.xlsx")
 data = data.set_index("date")
 data.index = pd.to_datetime(data.index)
 print(data.shape)
-print(data.head(2))
-print(data.tail(2))
-data = data.dropna()
-print(data.tail(2))
+print(data.head(3))
+print(data.tail(3))
+data = data.dropna()  #删除空格行
+print(data.tail(3))
 
 
-plt.figure(figsize=(10,5))  #设置画布大小
+plt.figure(figsize=(10,5), dpi=100)  #设置画布大小
 
-plt.plot(data, label="data")  #往添加数据
-data1 = data.rolling(4).mean()
-plt.plot(data1, label="rolling_mean")
+plt.plot(data, color="#0000ff", label="4G")  #往添加数据
+plt.legend(loc="best")
 
-diff = data.diff(1)
-plt.plot(diff, color="#00ff00",label="diff")
+data_mean = data.rolling(4).mean()
+plt.plot(data_mean, color="#ff0000", label="4G_rolling_mean")
+plt.legend(loc="best")
+
+diff1 = data_mean.diff(1).dropna()
+print(diff1.head(3))
+plt.plot(diff1, color="#00ff00",label="4G_rolling_mean_diff1")
 plt.legend(loc="best")
 plt.show()
 
--- timeseries_predict.py
+++ timeseries_predict.py
@@ -1,9 +1,13 @@
 import pandas
-import statsmodels.api as sm
+import numpy
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
-data1 = [10930, 10318, 10595, 10972, 7706, 6756, 9092, 10551, 9722, 10913,
-        11151, 8186, 6422, 6337, 11649, 11652, 10310, 12043, 7937, 6476, 9662, 9570, 9981, 9331, 9449,
+data1 = [10930, 10318, 10595, 10972, 7706,
+        6756, 9092, 10551, 9722, 10913,
+        11151, 8186, 6422, 6337, 11649,
+        11652, 10310, 12043, 7937, 6476,
+        9662, 9570, 9981, 9331, 9449,
         6773, 6304, 9355, 10477, 10148,
         10395, 11261, 8713, 7299, 10424,
         10795, 11069, 11602, 11427, 9095,
@@ -20,36 +24,35 @@ data1 = [10930, 10318, 10595, 10972, 7706, 6756, 9092, 10551, 9722, 10913,
         ]
 
 data = pandas.Series(data1)
+data.index = pandas.Index(sm.tsa.datetools.dates_from_range('1926', '2015'))
+print(data.head(3))
 
-data.index = pandas.Index(
-    sm.tsa.datetools.dates_from_range('1926', '2015')
-)
+data.plot(figsize=(10, 5), label="data_orogin")
+plt.legend(loc="best")
 
-data.plot(figsize=(10, 6))
+#第一步：确定d，根据diff1和diff2的图像看，两种差不多，故选择d=1
+diff_1 = data.diff(1).dropna()
+diff_1.plot(figsize=(10,5), label="data_diff1")
+plt.legend(loc="best")
+
+diff_2 = data.diff(2).dropna()
+diff_2.plot(figsize=(10,5), label="data_diff2")
+plt.legend(loc="best")
 plt.show()
 
-#第一步、确定d
-diff_1 = data.diff(1)
-diff_1.plot(figsize=(10,6))
-plt.show()
-
-diff_2 = data.diff(2)
-diff_2.plot(figsize=(10,6))
-plt.show()
-
-arma_mode = sm.tsa.ARMA(diff_1.dropna(), (7, 0)).fit()
+#d=1
+arma_mode = sm.tsa.ARMA(diff_1, (8, 0)).fit()
 arma_predict = arma_mode.predict('2015', '2050', dynamic=True)
-#print(arma_predict)
 
-fig, ax = plt.subplots(figsize=(10,6))
+fig, ax = plt.subplots(figsize=(10,5))
 ax = diff_1.ix['1926':].plot(ax=ax)
 arma_predict.plot(ax=ax)
 plt.show()
 
 #第三步、将预测的差分值还原为
 lastValue = data[len(data)-2]
-print(lastValue)
-print("+++++++++")
+#print(lastValue)
+#print("+++++++++")
 for p in arma_predict:
     lastValue = lastValue + p
     #print(p)
