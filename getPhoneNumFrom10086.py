951e7e43eee672f0f74bf1f17a8b7b73d6c4a155
zhangyang <zhangyang1@hi.chinamobile.com>
2018-08-16 16:37:37 +0800

    优化

--- getPhoneNumFrom10086.py
+++ getPhoneNumFrom10086.py
@@ -26,17 +26,17 @@ def getInfo(html, info):
     pattern = re.compile(info, re.S)
     return pattern.findall(html)
 
-def openFile(fileDir="data", fileName="output.txt"):
+def openFile(fileDir="output", fileName="output.txt"):
     if not os.path.exists(fileDir):
         print(fileDir + "目录不存在，新建目录")
         os.mkdir(fileDir)
     os.chdir(os.path.abspath(fileDir)) #切换目录，将结果放在该目录下
     fp = open(fileName, "wb+") #结果保存在该文件中
+    os.chdir(os.path.abspath(".."))  # os.path.abspath返回绝对路径
     return fp
 
 def getZoneNum(file_name, sheet_name):
-    #os.chdir("data")
-    #file_name = "全国地市区号表.xlsx"
+    os.chdir(os.path.abspath("input"))  # os.path.abspath返回绝对路径
     excel_file = openpyxl.load_workbook(file_name)
     excel_sheet1 = excel_file[sheet_name]  #根据表名获取
     data=[]
@@ -46,6 +46,7 @@ def getZoneNum(file_name, sheet_name):
             data_row.append(cell.value)
         data.append(data_row)
     excel_file.close()
+    os.chdir(os.path.abspath(".."))  # os.path.abspath返回绝对路径
     return data
 
 def spider(url, zoneData):
@@ -77,18 +78,19 @@ def main():
                "https://shop.10086.cn/list/134_898_898_0_0_0_0_6_0.html",  #尾号ABCD
                "https://shop.10086.cn/list/134_898_898_0_0_0_0_7_0.html",  #尾号ABAC
     ]
+    zoneNum = getZoneNum("AllCityZoneList.xlsx", 'Sheet2')
     filename = "phoneNum_" + time.strftime("%Y%m%d%H%M%S") + ".txt"
-    fp = openFile("data", filename)
-    zoneNum = getZoneNum("AllCityZoneList.xlsx", 'Sheet1')
-    for each in zoneNum:
-        url = getZoneUrl(initUrl[2], each[1], each[3])
-        #print(url)
-        totalPhoneNum, data = spider(url, each)
-        for one in data:
-            print(one)
-            fp.write((one+'\n').encode('utf-8'))
-        print(each[0]+" "+each[2]+"共计"+str(totalPhoneNum)+"个号码")
-        time.sleep(2)  # 延时2秒
+    fp = openFile("output", filename)
+    for i in range(1,8):
+        for each in zoneNum:
+            zoneUrl = getZoneUrl(initUrl[i], each[1], each[3])
+            print(zoneUrl)
+            totalPhoneNum, data = spider(zoneUrl, each)
+            for one in data:
+                print(one)
+                fp.write((one+'\n').encode('utf-8'))
+            print(each[0]+" "+each[2]+"共计"+str(totalPhoneNum)+"个号码")
+            time.sleep(2)  # 延时2秒
     fp.close
 
 if __name__ == '__main__':
